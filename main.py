556089e5ea1d056b99577d0bc1c2cdd3d08f153c
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-14 13:51:36 +0530

    window setup using Tkinter

--- main.py
+++ main.py
@@ -12,4 +12,14 @@ window = Tk()
 window.title("Kanye says...")
 window.config(pady=50, padx=50)
 
-canvsa
+canvas = Canvas(width= 300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150,207, image=background_img)
+quote_text = canvas.create_text(150,207,text="Kanye quote goes here", width=250,font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img= PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img,highlightthickness=0,command=get_quote)
+kanye_button.grid(row=1,column=0)
+
+window.mainloop()
